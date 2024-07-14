0fdbab6275dafe8c975cc20c472830ac1408b994
MarceleLouiseSAra <louise20marcele03@gmail.com>
2024-07-14 15:39:38 -0300

    feat: atualização tictactoe.cpp

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -16,16 +16,53 @@ void TicTacToe::validaJogada(int x, int y, char z) {
 
 int TicTacToe::confereGanhador() {
   for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++) {
-      if (p[i][j] == p[i][j + 1] && p[i][j] == p[i][j + 2] && p[i][j] != ' ') {
-        cout << "entrei no primeiro if" << endl;
+    // valida as linhas horizontais:
+    if (p[i][0] == p[i][1] && p[i][0] == p[i][2] && p[i][0] != ' ') {
+      if (p[i][0] == 'X') { // caso o Jogador1 vença
         return 1;
       }
-      if (p[i][j] == p[i + 1][j] && p[i][j] == p[i + 2][j] && p[i][j] != ' ') {
-        cout << "entrei no segundo if" << endl;
-        return 1;
+      if (p[i][0] == 'O') { // caso o Jogador2 vença
+        return 2;
       }
     }
+    // valida as linhas verticais:
+    if (p[0][i] == p[1][i] && p[0][i] == p[2][i] && p[0][i] != ' ') {
+      if (p[0][i] == 'X') { // caso o Jogador1 vença
+        return 1;
+      }
+      if (p[0][i] == 'O') { // caso o Jogador2 vença
+        return 2;
+      }
+    }
+  }
+  // validam as linhas diagonais:
+  if (p[0][0] == p[1][1] && p[0][0] == p[2][2] && p[0][0] != ' ') {
+    if (p[0][0] == 'X') { // caso o Jogador1 vença
+      return 1;
+    }
+    if (p[0][0] == 'O') { // caso o Jogador2 vença
+      return 2;
+    }
+  }
+  if (p[0][2] == p[1][1] && p[0][2] == p[2][2] && p[2][0] != ' ') {
+    if (p[0][2] == 'X') { // caso o Jogador1 vença
+      return 1;
+    }
+    if (p[0][2] == 'O') { // caso o Jogador2 vença
+      return 2;
+    }
+  }
+  // caso ninguém vença
+  int i, j, a = 0;
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < columns; j++) {
+      if (p[i][j] == ' ') {
+        a = 1;
+      }
+    }
+  }
+  if (!a) {
+    return 3;
   }
   return 0;
 }
