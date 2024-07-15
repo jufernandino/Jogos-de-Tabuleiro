67ceb940b192c4c00e22ccfbc25f5950bb6ab7ca
Bárbara <154363062+barbaradinizabreu@users.noreply.github.com>
2024-07-15 14:51:37 +0000

    Pseudo-código do Reversi

---
+++ include/Reversi.hpp
@@ -0,0 +1,28 @@
+#ifndef Reversi_H
+#define Reversi_H
+#include "Tabuleiro.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+
+class Reversi : public Tabuleiro {
+  public: 
+    Reversi();
+
+    void inicializaTabuleiro(); //específica do Reversi
+
+    virtual imprimeTabuleiro() override; //não sei se ela já imprime com as mudanças de cada jogada, conferir
+
+    void defineJogada(); //lê entrada do usuário e guarda pra depois, não sei se vamos usar, conferir
+
+    virtual void validaJogada() override; //puxa do Tabuleiro.hpp + acrescenta especificidades do jogo, conferir
+
+    virtual int confereGanhador() override; //mesma coisa
+};
+
+*/
+
+#endif
---
+++ src/Reversi.cpp
@@ -0,0 +1,48 @@
+#include "Reversi.hpp"
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+/*
+Reversi::Reversi() {
+  this -> rows = 8;
+  this -> columns = 8;
+}
+
+void Reversi::inicializaTabuleiro() {
+  //lógica
+}
+
+void Reversi::defineJogada() {
+}
+
+void Reversi::validaJogada() {
+  3 casas: black, white, empty
+  black++ //usar apontadores aqui talvez???
+  white++
+}
+
+int Reversi::confereGanhador {
+
+//primeiro caso: tabuleiro totalmente preenchido, jogador com mais peças vence
+
+  for (int i = 0; i < rows; i++) { 
+    for (int j = 0; j < columns; j++) {
+      if (não existir mais nenhuma casa empty) {
+        black > white ? return 1 : return 2;
+      }
+    }
+  }
+
+//segundo caso: tabuleiro não totalmente preenchido, mas não há mais jogadas possíveis para um dos jogadores, jogador que tem jogadas disponíveis continua até que preencha todo o tabuleiro ou fique sem jogadas disponíveis e passa pro próximo, talvez o empate também se encaixe aqui, conferir
+
+//terceiro caso: tabuleiro totalmente preenchido, empate, mesmo número de peças
+
+  for (int i = 0; i < rows; i++) { 
+    for (int j = 0; j < columns; j++) {
+      if (black == white) {
+        return 3;
+      }
+    }
+  } 
+*/
