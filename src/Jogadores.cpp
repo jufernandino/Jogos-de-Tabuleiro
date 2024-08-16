e998c258d64be907682c4e8fbe4922064770457b
Marcele "Sally" Gomes <louise20marcele03@gmail.com>
2024-08-16 19:27:34 -0300

    Tratamento de excessôes e Campo Minado funcionando corretamente.

--- src/Jogadores.cpp
+++ src/Jogadores.cpp
@@ -7,8 +7,7 @@
 
 using namespace std;
 
-Jogadores::Jogadores()
-{
+Jogadores::Jogadores() {
   this->Apelido = "";
   this->Nome = "";
   this->reversiWins = 0;
@@ -24,8 +23,7 @@ Jogadores::Jogadores()
   this->victory = false;
 }
 
-Jogadores::Jogadores(string apelido, string nome)
-{
+Jogadores::Jogadores(string apelido, string nome) {
   this->Apelido = apelido;
   this->Nome = nome;
   this->reversiWins = 0;
@@ -41,52 +39,40 @@ Jogadores::Jogadores(string apelido, string nome)
   this->victory = false;
 
   ofstream out("Jogadores.txt", fstream::app);
-  if (out.is_open())
-  {
+  if (out.is_open()) {
     out << '\n'
-      << this->Apelido << ", " << this->Nome << ", " << this->reversiWins << ", " << this->reversiDefeats << ", "
-      << this->lig4Wins << ", " << this->lig4Defeats << ", "
-      << this->tictactoeWins << ", " << this->tictactoeDefeats << ", "
-      << this->campoMinadoWins << ", " << this->campoMinadoDefeats << ", "
-      << this->memoriaWins << ", " << this->memoriaDefeats;
+        << this->Apelido << ", " << this->Nome << ", " << this->reversiWins
+        << ", " << this->reversiDefeats << ", " << this->lig4Wins << ", "
+        << this->lig4Defeats << ", " << this->tictactoeWins << ", "
+        << this->tictactoeDefeats << ", " << this->campoMinadoWins << ", "
+        << this->campoMinadoDefeats << ", " << this->memoriaWins << ", "
+        << this->memoriaDefeats;
     out.close();
-  }
-  else
-  {
+  } else {
     cout << "ERRO: não foi possível abrir o arquivo Jogadores.txt" << endl;
   }
 }
 
-int Jogadores::pesquisaJogador(string apelido)
-{
+int Jogadores::pesquisaJogador(string apelido) {
 
   ifstream in("Jogadores.txt", fstream::in);
 
-  if (in.is_open())
-  {
+  if (in.is_open()) {
     string line;
-    while (getline(in, line))
-    {
+    while (getline(in, line)) {
       string aux = "";
-      for (long unsigned int i = 0; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (long unsigned int i = 0; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
-      if (aux == apelido)
-      {
+      if (aux == apelido) {
         return 1;
       }
     }
-  }
-  else
-  {
+  } else {
     cout << "ERRO: não foi possível abrir o arquivo Jogadores.txt" << endl;
     return 0;
   }
@@ -95,14 +81,11 @@ int Jogadores::pesquisaJogador(string apelido)
   return 0;
 }
 
-bool Jogadores::logar(string Apelido, vector<Jogadores> &jogadoresVector)
-{
+Jogadores Jogadores::logIn(string Apelido, vector<Jogadores> &jogadoresVector) {
 
   vector<Jogadores>::iterator it;
-  for (it = jogadoresVector.begin(); it != jogadoresVector.end(); it++)
-  {
-    if ((*it).Apelido == Apelido)
-    {
+  for (it = jogadoresVector.begin(); it != jogadoresVector.end(); it++) {
+    if ((*it).Apelido == Apelido) {
       this->Apelido = (*it).Apelido;
       this->Nome = (*it).Nome;
       this->reversiWins = (*it).reversiWins;
@@ -115,58 +98,50 @@ bool Jogadores::logar(string Apelido, vector<Jogadores> &jogadoresVector)
       this->campoMinadoDefeats = (*it).campoMinadoDefeats;
       this->memoriaWins = (*it).memoriaWins;
       this->memoriaDefeats = (*it).memoriaDefeats;
-      return true;
+      return *this;
     }
   }
-  return false;
+  return *this;
 }
 
-void Jogadores::reescreveArquivo(vector<Jogadores> &jogadoresVector)
-{
-  ofstream out("Jogadores.txt", ios::out | ios::trunc); // Abre o arquivo apaga o conteúdo anterior
+void Jogadores::reescreveArquivo(vector<Jogadores> &jogadoresVector) {
+  ofstream out("Jogadores.txt",
+               ios::out |
+                   ios::trunc); // Abre o arquivo apaga o conteúdo anterior
 
-  if (!out.is_open())
-  {
+  if (!out.is_open()) {
     cout << "ERRO: não foi possível abrir o arquivo Jogadores.txt" << endl;
     return;
   }
 
   // Reescreve o arquivo com o conteúdo atualizado do vetor
-  for (const auto &jogador : jogadoresVector)
-  {
+  for (const auto &jogador : jogadoresVector) {
     out << jogador.Apelido << ", " << jogador.Nome << ", "
         << jogador.reversiWins << ", " << jogador.reversiDefeats << ", "
         << jogador.lig4Wins << ", " << jogador.lig4Defeats << ", "
         << jogador.tictactoeWins << ", " << jogador.tictactoeDefeats << ", "
-        << jogador.campoMinadoWins << ", " << jogador.campoMinadoDefeats << ","
+        << jogador.campoMinadoWins << ", " << jogador.campoMinadoDefeats << ", "
         << jogador.memoriaWins << ", " << jogador.memoriaDefeats << '\n';
   }
 
   out.close();
 }
 
-void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
-{
+void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector) {
   ifstream in("Jogadores.txt", fstream::in);
 
-  if (in.is_open())
-  {
+  if (in.is_open()) {
     string line;
-    while (getline(in, line))
-    {
+    while (getline(in, line)) {
 
       Jogadores jogador;
       string aux = "";
       long unsigned int i = 0;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -174,14 +149,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -189,14 +160,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -204,14 +171,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -219,14 +182,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -234,14 +193,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -249,14 +204,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -264,14 +215,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -279,14 +226,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -294,14 +237,10 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != ',')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != ',') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
@@ -309,22 +248,16 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
       aux = "";
       i += 2;
 
-      for (; i < line.size(); i++)
-      {
-        if (line[i] != '\n')
-        {
+      for (; i < line.size(); i++) {
+        if (line[i] != '\n') {
           aux = aux + line[i];
-        }
-        else
-        {
+        } else {
           break;
         }
       }
       jogadoresVector.push_back(jogador);
     }
-  }
-  else
-  {
+  } else {
     cout << "ERRO: não foi possível abrir o arquivo Jogadores.txt" << endl;
     return;
   }
@@ -332,17 +265,15 @@ void Jogadores::carregarJogadores(vector<Jogadores> &jogadoresVector)
   in.close();
 }
 
-void Jogadores::removeJogador(vector<Jogadores> &jogadoresVector, string &Apelido)
-{
+void Jogadores::removeJogador(vector<Jogadores> &jogadoresVector,
+                              string &Apelido) {
 
   vector<Jogadores>::iterator it;
   bool jogadorEncontrado = false;
 
   // Procura pelo jogador no vetor
-  for (it = jogadoresVector.begin(); it != jogadoresVector.end(); ++it)
-  {
-    if (it->Apelido == Apelido)
-    {
+  for (it = jogadoresVector.begin(); it != jogadoresVector.end(); ++it) {
+    if (it->Apelido == Apelido) {
       jogadoresVector.erase(it); // Remove o jogador do vetor
       jogadorEncontrado = true;
       break;
@@ -350,118 +281,79 @@ void Jogadores::removeJogador(vector<Jogadores> &jogadoresVector, string &Apelid
   }
 
   // Se o jogador foi encontrado e removido do vetor, reescreva o arquivo
-  if (jogadorEncontrado)
-  {
-    ofstream outFile("Jogadores.txt", ios::out | ios::trunc); // Abre o arquivo e exclui o conteúdo anterior
+  if (jogadorEncontrado) {
+    ofstream outFile(
+        "Jogadores.txt",
+        ios::out | ios::trunc); // Abre o arquivo e exclui o conteúdo anterior
 
-    if (!outFile.is_open())
-    {
+    if (!outFile.is_open()) {
       cout << "ERRO: não foi possível abrir o arquivo Jogadores.txt" << endl;
       return;
     }
 
-    // Reescreve o arquivo com o conteúdo atualizado do vetor
-    for (const auto &jogador : jogadoresVector)
-    {
-      outFile << jogador.Apelido << ", " << jogador.Nome << ", "
-              << jogador.reversiWins << ", " << jogador.reversiDefeats << ", "
-              << jogador.lig4Defeats << ", "
-              << jogador.tictactoeWins << ", " << jogador.tictactoeDefeats << ", "
-              << jogador.campoMinadoWins << ", " << jogador.campoMinadoDefeats << ", "
-              << jogador.memoriaWins << ", " << jogador.memoriaDefeats << '\n';
-    }
+    Jogadores::reescreveArquivo(jogadoresVector);
 
     outFile.close();
 
     cout << "Jogador " << Apelido << " removido com sucesso" << endl;
-  }
-  else
-  {
+
+  } else {
+
     cout << "ERRO: jogador inexistente" << endl;
   }
 }
 
 void Jogadores::atualizaEstatisticas(char jogoEscolhido,
-                                     vector<Jogadores> &jogadoresVector)
-{
+                                     vector<Jogadores> &jogadoresVector) {
   vector<Jogadores>::iterator it;
-  for (it = jogadoresVector.begin(); it != jogadoresVector.end(); it++)
-  {
-    if ((*it).Apelido == this->Apelido)
-    {
-      if (jogoEscolhido == 'R')
-      {
-        if (this->victory)
-        {
+  for (it = jogadoresVector.begin(); it != jogadoresVector.end(); it++) {
+    if ((*it).Apelido == this->Apelido) {
+      if (jogoEscolhido == 'R') {
+        if (this->victory) {
           (*it).reversiWins++;
-        }
-        else if (!this->victory)
-        {
+        } else if (!this->victory) {
           (*it).reversiDefeats++;
         }
         break;
-      }
-      else if (jogoEscolhido == 'L')
-      {
-        if (this->victory)
-        {
+      } else if (jogoEscolhido == 'L') {
+        if (this->victory) {
           (*it).lig4Wins++;
-        }
-        else if (!this->victory)
-        {
+        } else if (!this->victory) {
           (*it).lig4Defeats++;
         }
         break;
-      }
-      else if (jogoEscolhido == 'T')
-      {
-        if (this->victory)
-        {
+      } else if (jogoEscolhido == 'T') {
+        if (this->victory) {
           (*it).tictactoeWins++;
-        }
-        else if (!this->victory)
-        {
+        } else if (!this->victory) {
           (*it).tictactoeDefeats++;
         }
         break;
-      }
-      else if (jogoEscolhido == 'C')
-      {
-        if (this->victory)
-        {
+      } else if (jogoEscolhido == 'C') {
+        if (this->victory) {
           (*it).campoMinadoWins++;
-        }
-        else if (!this->victory)
-        {
+        } else if (!this->victory) {
           (*it).campoMinadoDefeats++;
         }
-      }
-      else if (jogoEscolhido == 'M')
-      {
-        if (this->victory)
-        {
+      } else if (jogoEscolhido == 'M') {
+        if (this->victory) {
           (*it).memoriaWins++;
-        }
-        else if (!this->victory)
-        {
+        } else if (!this->victory) {
           (*it).memoriaDefeats++;
         }
       }
     }
   }
-  this->reescreveArquivo(jogadoresVector);
+  Jogadores::reescreveArquivo(jogadoresVector);
 }
 
 // método para mostrar o raking de cada jogo, de acordo com o número de vitórias
 void Jogadores::mostrarRanking(const char &jogoEscolhido,
-                               vector<Jogadores> jogadoresVector)
-{
+                               vector<Jogadores> &jogadoresVector) {
 
   auto ordenacaoVitorias = [jogoEscolhido](const Jogadores &x,
-                                           const Jogadores &y)
-  {
-    switch (jogoEscolhido)
-    {
+                                           const Jogadores &y) {
+    switch (jogoEscolhido) {
     case 'R':
       return (x.reversiWins > y.reversiWins);
       break;
@@ -489,14 +381,11 @@ void Jogadores::mostrarRanking(const char &jogoEscolhido,
 
   sort(jogadoresVector.begin(), jogadoresVector.end(), ordenacaoVitorias);
 
-  cout << "\n--------\nRANKING DO JOGO\n--------\n"
-       << endl;
+  cout << "\n--------\nRANKING DO JOGO\n--------\n" << endl;
 
-  for (const auto &jogador : jogadoresVector)
-  {
+  for (const auto &jogador : jogadoresVector) {
 
-    switch (jogoEscolhido)
-    {
+    switch (jogoEscolhido) {
     case 'R':
       cout << jogador.Apelido << ", " << jogador.Nome << ", "
            << jogador.reversiWins << " vitória(s)" << endl;
@@ -525,77 +414,69 @@ void Jogadores::mostrarRanking(const char &jogoEscolhido,
       cout << "ERRO: opção inválida" << endl;
     }
   }
-  cout << "\n"
-       << endl;
+  cout << "\n" << endl;
 }
 
 void Jogadores::mostrarEstatisticas(vector<Jogadores> jogadoresVector,
-                                    const string &ordenacao)
-{
+                                    const string &ordenacao) {
 
   int tamanho = jogadoresVector.size();
 
   // selection sort
-  if (ordenacao == "A" || ordenacao == "a")
-  { // ordenação por apelido
-    for (int i = 0; i < tamanho - 1; ++i)
-    {
+  if (ordenacao == "A" || ordenacao == "a") { // ordenação por apelido
+    for (int i = 0; i < tamanho - 1; ++i) {
       int menorElemento = i;
-      for (int j = i + 1; j < tamanho; ++j)
-      {
+      for (int j = i + 1; j < tamanho; ++j) {
         if (jogadoresVector[j].Apelido <
-            jogadoresVector[menorElemento].Apelido)
-        {
+            jogadoresVector[menorElemento].Apelido) {
           menorElemento = j;
         }
       }
       swap(jogadoresVector[i], jogadoresVector[menorElemento]);
     }
-  }
-  else if (ordenacao == "N" || ordenacao == "n")
-  { // ordenação por nome
-    for (int i = 0; i < tamanho - 1; ++i)
-    {
+  } else if (ordenacao == "N" || ordenacao == "n") { // ordenação por nome
+    for (int i = 0; i < tamanho - 1; ++i) {
       int menorElemento = i;
-      for (int j = i + 1; j < tamanho; ++j)
-      {
-        if (jogadoresVector[j].Nome < jogadoresVector[menorElemento].Nome)
-        {
+      for (int j = i + 1; j < tamanho; ++j) {
+        if (jogadoresVector[j].Nome < jogadoresVector[menorElemento].Nome) {
           menorElemento = j;
         }
       }
       swap(jogadoresVector[i], jogadoresVector[menorElemento]);
     }
+  } else {
+    cout << "ERRO: comando inválido; escolha ordenar por (A)pelido ou (N)ome!"
+         << endl;
+    return;
   }
-  for (const auto &jogador : jogadoresVector)
-  {
+  for (const auto &jogador : jogadoresVector) {
     cout << "\n";
     cout << jogador.Apelido << " " << jogador.Nome << endl;
-    cout << "REVERSI - V: " << jogador.reversiWins << " D: " << jogador.reversiDefeats << endl;
-    cout << "LIG 4 - V: " << jogador.lig4Wins << " D: " << jogador.lig4Defeats << endl;
-    cout << "TIC TAC TOE - V: " << jogador.tictactoeWins << " D: " << jogador.tictactoeDefeats << endl;
-    cout << "CAMPO MINADO - V: " << jogador.campoMinadoWins << " D: " << jogador.campoMinadoDefeats << endl;
-    cout << "JOGO DA MEMORIA - V: " << jogador.memoriaWins << " D: " << jogador.memoriaDefeats << endl;
+    cout << "REVERSI - V: " << jogador.reversiWins
+         << " D: " << jogador.reversiDefeats << endl;
+    cout << "LIG 4 - V: " << jogador.lig4Wins << " D: " << jogador.lig4Defeats
+         << endl;
+    cout << "TIC TAC TOE - V: " << jogador.tictactoeWins
+         << " D: " << jogador.tictactoeDefeats << endl;
+    cout << "CAMPO MINADO - V: " << jogador.campoMinadoWins
+         << " D: " << jogador.campoMinadoDefeats << endl;
+    cout << "JOGO DA MEMORIA - V: " << jogador.memoriaWins
+         << " D: " << jogador.memoriaDefeats << endl;
   }
 }
 
 void Jogadores::cadastrarJogadores(string apelido, string nome,
                                    Jogadores &Jogador,
-                                   vector<Jogadores> &jogadoresVector)
-{
+                                   vector<Jogadores> &jogadoresVector) {
 
   Jogadores aux;
 
-  if (aux.pesquisaJogador(apelido))
-  { // se jogador já existir
+  if (aux.pesquisaJogador(apelido)) { // se jogador já existir
     cout << "ERRO: jogador repetido" << endl;
     return;
-  }
-  else
-  {
+  } else {
     Jogadores novoJogador(apelido, nome);
-    if (novoJogador.Apelido != "")
-    {
+    if (novoJogador.Apelido != "") {
       jogadoresVector.push_back(novoJogador);
       Jogador = novoJogador;
       cout << "\nJogador " << novoJogador.Apelido << " cadastrado com sucesso"
