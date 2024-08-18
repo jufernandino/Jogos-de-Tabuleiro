1a1aa8621207d142e8981eadc64fd36a5c3eccc4
Isadora Horta <dorarates19@gmail.com>
2024-08-18 18:32:17 -0300

    Ajuste na documentação da classe tabuleiro

--- include/Tabuleiro.hpp
+++ include/Tabuleiro.hpp
@@ -11,9 +11,7 @@ using namespace std;
  * @brief Classe base para representar um tabuleiro de jogo.
  *
  * A classe "Tabuleiro" fornece uma interface básica para criar, imprimir e manipular
- * um tabuleiro de jogo, bem como conferir o ganhador de uma partida.
- * Esta classe serve como base para tabuleiros específicos de jogos, permitindo
- * herança e personalização.
+ * um tabuleiro de jogo. É uma classe base para o tabuleiro dos jogos.
  */
 class Tabuleiro
 {
@@ -25,44 +23,47 @@ public:
   /**
    * @brief Construtor padrão da classe Tabuleiro.
    *
-   * Inicializa os atributos `rows` e `columns` com valores padrão e
-   * prepara a matriz para representar o tabuleiro.
+   * Inicializa os atributos "rows" e "columns".
    */
   Tabuleiro();
 
   /**
    * @brief Cria o tabuleiro alocando memória dinamicamente para a matriz.
    *
-   * Esta função aloca uma matriz 2D de caracteres que será usada como
-   * tabuleiro de jogo, com as dimensões especificadas pelos atributos `rows` e `columns`.
+   * Aloca a matriz que será utilizada como tabuleiro.
+   * 
+   * @retval none
    */
   void criaTabuleiro(); // aloca uma matriz dinamicamente que servirá como tabuleiro
 
   /**
    * @brief Imprime o tabuleiro na tela.
    *
-   * Esta função imprime o tabuleiro com os caracteres de separação como `-` e `|`
-   * para representar as células do tabuleiro.
+   * Esta função imprime o tabuleiro.
+   * 
+   * @retval none
    */
   virtual void imprimirTabuleiro(); // imrpime o tabuleiro com -s e |s
 
   /**
    * @brief Valida uma jogada no tabuleiro.
    *
-   * Verifica se os valores inseridos para a jogada estão dentro das dimensões do tabuleiro
-   * e, se válidos, efetua a jogada no tabuleiro.
+   * Verifica se os valores inseridos para a jogada estão dentro das dimensões do tabuleiro.
    *
-   * @param x Coordenada da linha da jogada.
-   * @param y Coordenada da coluna da jogada.
-   * @param z Caractere que representa o jogador (ex: 'X' ou 'O').
+   * @param x int 
+   * @param y int
+   * @param z int
+   * 
+   * @retval none
    */
   virtual void validaJogada(int x, int y, char z); // confere se os valores inseridos são menores que as dimensôes do tabuleiro e efetua a jogada
 
   /**
    * @brief Libera a memória alocada para o tabuleiro.
    *
-   * Esta função libera a memória previamente alocada para a matriz do tabuleiro,
-   * evitando vazamentos de memória.
+   * Esta função libera a memória previamente alocada.
+   * 
+   * @retval none
    */
   void liberaMemoria(); // libera a memória alocada anteriomente em criaTabuleiro()
 
@@ -72,16 +73,18 @@ public:
    * Função virtual pura que deve ser implementada pelas classes derivadas para
    * verificar se há um vencedor na partida.
    *
-   * @return Um inteiro que indica o resultado da conferência (1 para vitória, 0 para nenhum vencedor).
+   * @retval int
    */
   virtual int confereGanhador() = 0; /// confere se alguém venceu
 
   /**
    * @brief Mostra as regras do jogo correspondente.
    *
-   * Esta função exibe as regras básicas do jogo escolhido pelo jogador.
+   * Esta função exibe as regras jogo escolhido pelo jogador.
    *
-   * @param jogoEscolhido Caracter que identifica o jogo escolhido.
+   * @param jogoEscolhido char
+   * 
+   * @retval none
    */
   void mostrarRegras(const char &jogoEscolhido);
 };
